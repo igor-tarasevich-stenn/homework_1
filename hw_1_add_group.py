f16623ebd97f2cfbf00b3934ed03d40d6030248c
igor-tarasevich-stenn <igor.tarasevich@stenn.com>
2021-12-20 02:08:19 +0500

    Настроено виртуальное окружение, добавлены билиотеки selenium и pytest. Произведен успешный запуск тест из консоли и из среды разработки

--- hw_1_add_group.py
+++ hw_1_add_group.py
@@ -7,11 +7,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
+
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-    
+
     def test_untitled_test_case(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
@@ -35,18 +36,21 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
 
-    
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
     def tearDown(self):
         self.wd.quit()
 
