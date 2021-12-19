2e33065ef87f71f3818f316aeef50bd8d062b3e2
igor-tarasevich-stenn <igor.tarasevich@stenn.com>
2021-12-20 04:09:12 +0500

    Использован вспомогательный класс в тесте

--- hw_1_add_group.py
+++ hw_1_add_group.py
@@ -16,7 +16,7 @@ class hw_1_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd)
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, Group(name="qwerty", header="qwerty", footer="qwerty"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -28,18 +28,18 @@ class hw_1_add_group(unittest.TestCase):
         # return to groups page
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, group):
         # create group
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("qwerty")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("qwerty")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("qwerty")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd):
