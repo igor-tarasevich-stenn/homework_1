f7c06051416df5dd604de5225c03bf8df96c0e04
igor-tarasevich-stenn <igor.tarasevich@stenn.com>
2021-12-20 06:15:43 +0500

    Исправлены предупреждения, касающиеся поиска элементов на странице

--- test_add_contact.py
+++ test_add_contact.py
@@ -5,63 +5,71 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
+
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
-    
     def test_add_contact(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/edit.php")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("name")
-        driver.find_element_by_name("lastname").click()
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("lastname")
-        driver.find_element_by_name("company").click()
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys("test")
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("123")
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("test@mail.com")
-        driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("15")
-        driver.find_element_by_xpath("//option[@value='15']").click()
-        driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("October")
-        driver.find_element_by_xpath("//option[@value='October']").click()
-        driver.find_element_by_name("byear").click()
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1995")
-        driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        driver.find_element_by_link_text("home page").click()
-        driver.find_element_by_link_text("Logout").click()
-    
+        wd = self.wd
+        # go to home page
+        wd.get("http://localhost/addressbook/edit.php")
+        # login
+        wd.find_element("name", "user").click()
+        wd.find_element("name", "user").clear()
+        wd.find_element("name", "user").send_keys("admin")
+        wd.find_element("name", "pass").clear()
+        wd.find_element("name", "pass").send_keys("secret")
+        wd.find_element("xpath", "//input[@value='Login']").click()
+        # add contact
+        wd.find_element("name", "firstname").click()
+        wd.find_element("name", "firstname").clear()
+        wd.find_element("name", "firstname").send_keys("name")
+        wd.find_element("name", "lastname").click()
+        wd.find_element("name", "lastname").clear()
+        wd.find_element("name", "lastname").send_keys("lastname")
+        wd.find_element("name", "company").click()
+        wd.find_element("name", "company").clear()
+        wd.find_element("name", "company").send_keys("test")
+        wd.find_element("name", "home").click()
+        wd.find_element("name", "home").clear()
+        wd.find_element("name", "home").send_keys("123")
+        wd.find_element("name", "email").click()
+        wd.find_element("name", "email").clear()
+        wd.find_element("name", "email").send_keys("test@mail.com")
+        wd.find_element("name", "bday").click()
+        Select(wd.find_element("name", "bday")).select_by_visible_text("15")
+        wd.find_element("xpath", "//option[@value='15']").click()
+        wd.find_element("name", "bmonth").click()
+        Select(wd.find_element("name", "bmonth")).select_by_visible_text("October")
+        wd.find_element("xpath", "//option[@value='October']").click()
+        wd.find_element("name", "byear").click()
+        wd.find_element("name", "byear").clear()
+        wd.find_element("name", "byear").send_keys("1995")
+        wd.find_element("xpath", "//div[@id='content']/form/input[21]").click()
+        # go back to home page
+        wd.find_element("link text", "home page").click()
+        # logout
+        wd.find_element("link text", "Logout").click()
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
 
-    
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
     def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
