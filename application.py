b03991378b4598efa4bfa0c360ba4fc61287b7b6
igor-tarasevich-stenn <igor.tarasevich@stenn.com>
2021-12-24 06:06:21 +0500

    Реализована функция создания фикстуры для контактов

---
+++ application.py
@@ -0,0 +1,67 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+
+
+class Application_contact:
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(30)
+
+    def go_to_home_page(self):
+        wd = self.wd
+        # go to home page
+        wd.get("http://localhost/addressbook/edit.php")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.go_to_home_page(wd)
+        # login
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def add_contact(self, contact):
+        wd = self.wd
+        # add contact
+        wd.find_element(By.NAME, "firstname").click()
+        wd.find_element(By.NAME, "firstname").clear()
+        wd.find_element(By.NAME, "firstname").send_keys(contact.name)
+        wd.find_element(By.NAME, "lastname").click()
+        wd.find_element(By.NAME, "lastname").clear()
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "company").click()
+        wd.find_element(By.NAME, "company").clear()
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
+        wd.find_element(By.NAME, "home").click()
+        wd.find_element(By.NAME, "home").clear()
+        wd.find_element(By.NAME, "home").send_keys(contact.home_tel)
+        wd.find_element(By.NAME, "email").click()
+        wd.find_element(By.NAME, "email").clear()
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
+        wd.find_element(By.NAME, "bday").click()
+        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
+        wd.find_element(By.NAME, "bmonth").click()
+        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element(By.NAME, "byear").click()
+        wd.find_element(By.NAME, "byear").clear()
+        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
+        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.wd
+        # return to home page
+        wd.find_element(By.LINK_TEXT, "home page").click()
+
+    def logout(self):
+        wd = self.wd
+        # logout
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def destroy(self):
+        self.wd.quit()
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,91 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
 from contact import Contact
-from selenium.webdriver.common.by import By
+from application import Application_contact
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.go_to_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.add_contact(wd, Contact(name="name1", lastname="lastname1", company="test1", home_tel="1231",
-                                     email="test1@mail.com", bday="11", bmonth="September", byear="1992"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        # logout
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def return_to_home_page(self, wd):
-        # return to home page
-        wd.find_element(By.LINK_TEXT, "home page").click()
-
-    def add_contact(self, wd, contact):
-        # add contact
-        wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(contact.name)
-        wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        wd.find_element(By.NAME, "company").click()
-        wd.find_element(By.NAME, "company").clear()
-        wd.find_element(By.NAME, "company").send_keys(contact.company)
-        wd.find_element(By.NAME, "home").click()
-        wd.find_element(By.NAME, "home").clear()
-        wd.find_element(By.NAME, "home").send_keys(contact.home_tel)
-        wd.find_element(By.NAME, "email").click()
-        wd.find_element(By.NAME, "email").clear()
-        wd.find_element(By.NAME, "email").send_keys(contact.email)
-        wd.find_element(By.NAME, "bday").click()
-        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
-        wd.find_element(By.NAME, "bmonth").click()
-        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element(By.NAME, "byear").click()
-        wd.find_element(By.NAME, "byear").clear()
-        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, username, password):
-        # login
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def go_to_home_page(self, wd):
-        # go to home page
-        wd.get("http://localhost/addressbook/edit.php")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
+def app(request):
+    fixture = Application_contact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.add_contact(Contact(name="name1", lastname="lastname1", company="test1", home_tel="1231",
+                                 email="test1@mail.com", bday="11", bmonth="September", byear="1992"))
+    app.logout()
+
