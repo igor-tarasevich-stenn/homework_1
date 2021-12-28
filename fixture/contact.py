8eb466be9eb7101fe914d88d00f4b0913ef2cdf0
igor-tarasevich-stenn <igor.tarasevich@stenn.com>
2021-12-28 07:55:54 +0500

    Создана общая для всех тестов фикстура в файле conftest.py

--- fixture/contact.py
+++ fixture/contact.py
@@ -41,6 +41,15 @@ class ContactHelper:
         wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element(By.NAME, "selected[]").click()
+        # init deletion
+        wd.find_element(By.XPATH, "//input[@value='Delete']").click()
+        wd.switch_to.alert().accept()
+        self.return_to_home_page()
+
     def return_to_home_page(self):
         wd = self.app.wd
         # return to home page
