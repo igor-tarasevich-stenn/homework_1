2c6627e0eacfc0aca0954211cfc2ab15e04a2a0e
igor-tarasevich-stenn <igor.tarasevich@stenn.com>
2022-03-07 08:23:36 +0500

    Промежуточный коммит реализации проверки в виде сравнения списков

--- model/contact.py
+++ model/contact.py
@@ -11,3 +11,9 @@ class Contact:
         self.bmonth = bmonth
         self.byear = byear
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.name, self.lastname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name and self.lastname == other.lastname
